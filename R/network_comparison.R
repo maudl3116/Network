1b67f83f92d289835464e682fb6eb48627ae3c5c
Maud LEMERCIER <ml3116@ic.ac.uk>
2018-10-14 13:24:27 +0100

    plot measures update

--- R/network_comparison.R
+++ R/network_comparison.R
@@ -59,13 +59,13 @@ F_measure <- 2/ ((1 / Precision) + (1 / Recall) )
 
 F_measure_plot <- function(k, n){
   A = sampleErdosRenyi(n, p=0.1)[[1]]
-  E <- interact(A,alpha=0.6,beta=0.009, n,k)
   SA = A[lower.tri(A)]
   Precision_vector <- 1:k
   Recall_vector <- 1:k
   F_measure_vector <- 1:k
   Accuracy_vector <- 1:k
   for (i in 1:k){
+    E <- interact(A,alpha=0.6,beta=0.009, n,i)
     simula1 <- EM(alpha0=0.4, beta0=0.02, rho0=0.15, n, i, E)
     Q <- as.matrix(simula1)[[1]]
     SQ=Q[lower.tri(Q)]
@@ -83,11 +83,11 @@ F_measure_plot <- function(k, n){
     Recall_vector[i] <- TP / (TP+FN)
     F_measure_vector[i] <- 2/ ((1 / Precision_vector[i]) + (1 / Recall_vector[i]))
   }
-  plot(F_measure_vector, type="l", xlab="Number of days", col="coral", ylab="",xlim=c(1,20), ylim=c(0,1),lwd=2)
-  lines(Accuracy_vector, type="l", xlab="Number of days",col="dark green", ylab="", lty=2,xlim=c(1,20) ,ylim=c(0,1),lwd=2)
-  lines(Precision_vector, type="l", xlab="Number of days",col="blue", ylab="", lty=3,xlim=c(1,20) ,ylim=c(0,1),lwd=2)
-  lines(Recall_vector, type="l", xlab="Number of days",col="dark violet", ylab="", lty=4,xlim=c(1,20) ,ylim=c(0,1),lwd=2)
-  legend("bottomright",legend=c("F Measure", "Accuracy", "Precision", "Recall"),lty=1:4,col=c("coral","dark green", "blue", "dark violet"), cex=0.9, y.intersp=0.4, lwd=2)
+  plot(F_measure_vector, type="l", xlab="Number of days", col="coral", ylab="",xlim=c(1,k), ylim=c(0,1),lwd=2)
+  lines(Accuracy_vector, type="l", xlab="Number of days",col="dark green", ylab="", lty=2,xlim=c(1,k) ,ylim=c(0,1),lwd=2)
+  lines(Precision_vector, type="l", xlab="Number of days",col="blue", ylab="", lty=3,xlim=c(1,k) ,ylim=c(0,1),lwd=2)
+  lines(Recall_vector, type="l", xlab="Number of days",col="dark violet", ylab="", lty=4,xlim=c(1,k) ,ylim=c(0,1),lwd=2)
+  legend("bottomright",legend=c("F Measure", "Accuracy", "Precision", "Recall"),lty=1:4,col=c("coral","dark green", "blue", "dark violet"), cex=1, y.intersp=0.6, lwd=2)
 }
 
 F_measure_plot(20, n=100)
--- R/simulation.R
+++ R/simulation.R
@@ -2,8 +2,8 @@
 #'
 #' This script allows you to simulate network data and perform inference via the Expectation Maximization algorithm
 
-n=50
-k=5
+n=5
+k=100
 p=0.1
 output <- sampleErdosRenyi(n,p)
 g <- output[[2]]
@@ -11,7 +11,7 @@ A <- output[[1]]
 alpha <- 0.6 # true positive rate
 beta <- 0.009 # false positive rate
 E <- interact(A,alpha=0.6,beta=0.009, n,k)
-E <- interact(A,alpha=0.6,beta=0.009, n,k)
+
 
 simulation1 <- EM(alpha0=0.4, beta0=0.02, rho0=0.15, n, k, E)
 
